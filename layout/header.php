35c1455b2001a2ab63ebcda96502ce7b6f167ac6
MUHAMMAD IQBAL BIN ZURIMI <18DIT24F1001@student.ptss.edu.my>
2026-03-25 23:38:56 +0800

    latest update

--- layout/header.php
+++ layout/header.php
@@ -5,7 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Biskut Klasik</title>
 
-<link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="css/style.css">
 
 </head>
 
